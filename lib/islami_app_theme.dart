d4fe6ed40d30c970fa57b196e574426a9543bd49
DELL <omnianabil1920@gmail.com>
2024-02-17 00:00:08 +0200

    sura details screen finished and theme

---
+++ lib/islami_app_theme.dart
@@ -0,0 +1,54 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MyThemeData {
+  static const Color primaryColor = Color(0xFFB7935F);
+  static const Color blackColor =Colors.black ;
+  static const Color whiteColor = Colors.white;
+  static ThemeData lightTheme = ThemeData(
+    brightness: Brightness.light,
+    primaryColor:primaryColor ,
+    dividerTheme: DividerThemeData(
+      color: primaryColor,
+      thickness: 3,
+    ),
+
+    textTheme: TextTheme(
+      bodySmall: GoogleFonts.inter(
+        fontSize: 25,
+        letterSpacing: .5,
+        fontWeight: FontWeight.w500,
+      ),
+      bodyMedium: GoogleFonts.elMessiri(
+        fontSize: 25,
+        fontWeight: FontWeight.w600,
+      ),
+      bodyLarge: GoogleFonts.inter(
+        fontSize: 30,
+        fontWeight: FontWeight.bold,
+      ),
+
+    ),
+    scaffoldBackgroundColor: Colors.transparent,
+    appBarTheme: AppBarTheme(
+      centerTitle: true,
+      backgroundColor: Colors.transparent,
+      titleTextStyle: GoogleFonts.elMessiri(
+        fontSize: 30,
+        fontWeight: FontWeight.bold,
+        color: blackColor,
+      ),
+    ),
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      selectedItemColor: blackColor,
+      showSelectedLabels: true,
+      showUnselectedLabels: false,
+      type: BottomNavigationBarType.fixed,
+      backgroundColor: primaryColor,
+      unselectedItemColor: whiteColor,
+    ),
+  );
+  static ThemeData darkTheme = ThemeData();
+}
---
+++ lib/model/sura_model.dart
@@ -0,0 +1,6 @@
+class SuraModel{
+  String name;
+  int index;
+
+  SuraModel({required this.name, required this.index});
+}

5507de9426c90a64a3749d74aaf8bc52fbb779ff
DELL <omnianabil1920@gmail.com>
2024-02-19 17:54:23 +0200

    Edit radio icons

--- lib/pages/home/tabs/radio_tab.dart
+++ lib/pages/home/tabs/radio_tab.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:islami_app/islami_app_theme.dart';
 import 'package:islami_app/providers/my_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -10,6 +11,10 @@ class RadioTab extends StatelessWidget {
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
     var provider = Provider.of<MyProvider>(context);
+    Color iconColor = theme.brightness == Brightness.light
+        ? MyThemeData.primaryColor
+        : MyThemeData.amberColor;
+    bool isTextDirectionRTL = Directionality.of(context) == TextDirection.rtl;
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
@@ -34,10 +39,13 @@ class RadioTab extends StatelessWidget {
           children: [
             Image(
               image: AssetImage(
-                provider.changeNextIc(),
+                isTextDirectionRTL
+                    ? 'assets/images/ic/Ic_metro-next.png'
+                    : 'assets/images/ic/Ic_metro-back.png',
               ),
               width: 13.5,
               height: 23.14,
+              color: iconColor,
             ),
             Icon(
               Icons.play_arrow_rounded,
@@ -46,10 +54,13 @@ class RadioTab extends StatelessWidget {
             ),
             Image(
               image: AssetImage(
-                provider.changeBackIc(),
+                isTextDirectionRTL
+                    ? 'assets/images/ic/Ic_metro-back.png'
+                    : 'assets/images/ic/Ic_metro-next.png',
               ),
               width: 13.5,
               height: 23.14,
+              color: iconColor,
             ),
           ],
         ),
